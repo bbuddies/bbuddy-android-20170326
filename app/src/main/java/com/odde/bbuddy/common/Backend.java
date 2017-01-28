c71cb6b3a43b322d41778c824ea8db73cf0454a6
Joseph Yao <joseph.yao.ruozhou@gmail.com>
2017-01-28 20:29:37 +0800

    refactoring: remove the middle man backend for authentication

--- app/src/main/java/com/odde/bbuddy/common/Backend.java
+++ app/src/main/java/com/odde/bbuddy/common/Backend.java
@@ -2,20 +2,15 @@ package com.odde.bbuddy.common;
 
 import android.content.Context;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.odde.bbuddy.account.Account;
 import com.odde.bbuddy.authentication.AuthenticationToken;
-import com.odde.bbuddy.authentication.Credentials;
 
 import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
 
 import java.io.IOException;
 import java.util.List;
-import java.util.Map;
 
 public class Backend {
 
@@ -26,25 +21,6 @@ public class Backend {
         jsonBackend = new JsonBackend(context);
     }
 
-    public void authenticate(Credentials credentials, final Consumer<String> afterSuccess) {
-        jsonBackend.postRequestForJson("/auth/sign_in", jsonOf(credentials), new Consumer<JSONObject>() {
-            @Override
-            public void accept(JSONObject jsonObject) {
-                afterSuccess.accept("success");
-            }
-        }, new Runnable() {
-            @Override
-            public void run() {
-                afterSuccess.accept("failed");
-            }
-        }, new Consumer<Map<String, String>>() {
-            @Override
-            public void accept(Map<String, String> headers) {
-                token.updateByHeaders(headers);
-            }
-        });
-    }
-
     public void processAllAccounts(final Consumer<List<Account>> consumer) {
         jsonBackend.getRequestForJsonArray("/accounts", token.getHeaders(), new Consumer<JSONArray>() {
             @Override
@@ -52,14 +28,6 @@ public class Backend {
                 consumer.accept(accountsFromJson(response));
             }
         });
-    }
-
-    private JSONObject jsonOf(Credentials credentials) {
-        try {
-            return new JSONObject(new ObjectMapper().writeValueAsString(credentials));
-        } catch (JsonProcessingException | JSONException e) {
-            throw new IllegalStateException(e);
-        }
     }
 
     private List<Account> accountsFromJson(JSONArray response) {
--- app/src/test/java/com/odde/bbuddy/authentication/AuthenticatorTest.java
+++ app/src/test/java/com/odde/bbuddy/authentication/AuthenticatorTest.java
@@ -1,13 +1,18 @@
 package com.odde.bbuddy.authentication;
 
-import com.odde.bbuddy.common.Backend;
 import com.odde.bbuddy.common.Consumer;
+import com.odde.bbuddy.common.JsonBackend;
 
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.junit.Test;
+import org.mockito.ArgumentCaptor;
 import org.mockito.invocation.InvocationOnMock;
 import org.mockito.stubbing.Answer;
 
+import static org.junit.Assert.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.doAnswer;
 import static org.mockito.Mockito.mock;
@@ -15,36 +20,40 @@ import static org.mockito.Mockito.verify;
 
 public class AuthenticatorTest {
 
-    Backend mockBackend = mock(Backend.class);
-    Authenticator authenticator = new Authenticator(mockBackend);
+    JsonBackend mockBackend = mock(JsonBackend.class);
+    AuthenticationToken mockAuthenticationToken = mock(AuthenticationToken.class);
+    Authenticator authenticator = new Authenticator(mockBackend, mockAuthenticationToken);
     Credentials credentials = new Credentials("abc@gmail.com", "password");
     Consumer afterSuccess = mock(Consumer.class);
 
     @Test
-    public void authenticate_with_user_name_and_password() throws Exception {
+    public void authenticate_with_user_name_and_password() throws JSONException {
         authenticator.authenticate(credentials, afterSuccess);
 
-        verify(mockBackend).authenticate(eq(credentials), any(Consumer.class));
+        ArgumentCaptor<JSONObject> captor = ArgumentCaptor.forClass(JSONObject.class);
+        verify(mockBackend).postRequestForJson(eq("/auth/sign_in"), captor.capture(), any(Consumer.class), any(Runnable.class), any(Consumer.class));
+        assertEquals("abc@gmail.com", captor.getValue().getString("email"));
+        assertEquals("password", captor.getValue().getString("password"));
     }
 
     @Test
     public void authenticate_successful() {
-        given_backend_will_success_with_message("login success");
+        given_jsonbackend_will_successfully_response();
 
         authenticator.authenticate(credentials, afterSuccess);
 
-        verify(afterSuccess).accept("login success");
+        verify(afterSuccess).accept("success");
     }
 
-    private void given_backend_will_success_with_message(final String message) {
+    private void given_jsonbackend_will_successfully_response() {
         doAnswer(new Answer() {
             @Override
             public Object answer(InvocationOnMock invocation) throws Throwable {
-                Consumer consumer = invocation.getArgument(1);
-                consumer.accept(message);
+                Consumer consumer = invocation.getArgument(2);
+                consumer.accept(new JSONObject());
                 return null;
             }
-        }).when(mockBackend).authenticate(any(Credentials.class), any(Consumer.class));
+        }).when(mockBackend).postRequestForJson(anyString(), any(JSONObject.class), any(Consumer.class), any(Runnable.class), any(Consumer.class));
     }
 
 }

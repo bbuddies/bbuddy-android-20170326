8cdecf4875d183934e4c81f626b747130e0e3231
Joseph Yao <joseph.yao.ruozhou@gmail.com>
2017-01-28 20:41:27 +0800

    make up missing unit tests for authenticator

--- app/src/test/java/com/odde/bbuddy/authentication/AuthenticatorTest.java
+++ app/src/test/java/com/odde/bbuddy/authentication/AuthenticatorTest.java
@@ -1,5 +1,7 @@
 package com.odde.bbuddy.authentication;
 
+import android.support.annotation.NonNull;
+
 import com.odde.bbuddy.common.Consumer;
 import com.odde.bbuddy.common.JsonBackend;
 
@@ -7,8 +9,11 @@ import org.json.JSONException;
 import org.json.JSONObject;
 import org.junit.Test;
 import org.mockito.ArgumentCaptor;
+import org.mockito.ArgumentMatchers;
 import org.mockito.invocation.InvocationOnMock;
 import org.mockito.stubbing.Answer;
+
+import java.util.HashMap;
 
 import static org.junit.Assert.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
@@ -28,7 +33,7 @@ public class AuthenticatorTest {
 
     @Test
     public void authenticate_with_user_name_and_password() throws JSONException {
-        authenticator.authenticate(credentials, afterSuccess);
+        authenticate();
 
         ArgumentCaptor<JSONObject> captor = ArgumentCaptor.forClass(JSONObject.class);
         verify(mockBackend).postRequestForJson(eq("/auth/sign_in"), captor.capture(), any(Consumer.class), any(Runnable.class), any(Consumer.class));
@@ -36,24 +41,65 @@ public class AuthenticatorTest {
         assertEquals("password", captor.getValue().getString("password"));
     }
 
+    private void authenticate() {
+        authenticator.authenticate(credentials, afterSuccess);
+    }
+
     @Test
     public void authenticate_successful() {
-        given_jsonbackend_will_successfully_response();
+        given_jsonbackend_will_response(success());
 
-        authenticator.authenticate(credentials, afterSuccess);
+        authenticate();
 
         verify(afterSuccess).accept("success");
     }
 
-    private void given_jsonbackend_will_successfully_response() {
-        doAnswer(new Answer() {
+    @Test
+    public void authenticate_failed() {
+        given_jsonbackend_will_response(failed());
+
+        authenticate();
+
+        verify(afterSuccess).accept("failed");
+    }
+
+    @Test
+    public void update_authentication_token() {
+        given_jsonbackend_will_response(success());
+
+        authenticate();
+
+        verify(mockAuthenticationToken).updateByHeaders(ArgumentMatchers.<String, String>anyMap());
+    }
+
+    @NonNull
+    private Answer failed() {
+        return new Answer() {
+            @Override
+            public Object answer(InvocationOnMock invocation) throws Throwable {
+                Runnable runnable = invocation.getArgument(3);
+                runnable.run();
+                return null;
+            }
+        };
+    }
+
+    private void given_jsonbackend_will_response(Answer answer) {
+        doAnswer(answer).when(mockBackend).postRequestForJson(anyString(), any(JSONObject.class), any(Consumer.class), any(Runnable.class), any(Consumer.class));
+    }
+
+    @NonNull
+    private Answer success() {
+        return new Answer() {
             @Override
             public Object answer(InvocationOnMock invocation) throws Throwable {
                 Consumer consumer = invocation.getArgument(2);
                 consumer.accept(new JSONObject());
+                Consumer headerConsumer = invocation.getArgument(4);
+                headerConsumer.accept(new HashMap<>());
                 return null;
             }
-        }).when(mockBackend).postRequestForJson(anyString(), any(JSONObject.class), any(Consumer.class), any(Runnable.class), any(Consumer.class));
+        };
     }
 
 }

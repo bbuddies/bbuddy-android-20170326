caa59d888803c3114d35c2c53ff3b57744dda977
Joseph Yao <joseph.yao.ruozhou@gmail.com>
2017-01-29 14:30:35 +0800

    make up missing unit tests for accounts

--- app/src/main/java/com/odde/bbuddy/account/Account.java
+++ app/src/main/java/com/odde/bbuddy/account/Account.java
@@ -1,14 +1,20 @@
 package com.odde.bbuddy.account;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class Account {
-    private String name;
 
-    @JsonProperty("balance")
+    private String name;
     private int balanceBroughtForward;
+
+    @JsonCreator
+    public Account(@JsonProperty("name") String name, @JsonProperty("balance") int balanceBroughtForward) {
+        this.name = name;
+        this.balanceBroughtForward = balanceBroughtForward;
+    }
 
     public String getName() {
         return name;

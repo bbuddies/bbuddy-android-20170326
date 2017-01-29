05ebe3401e8aaafb536be1c707c2104041b4f762
Joseph Yao <joseph.yao.ruozhou@gmail.com>
2017-01-29 21:25:18 +0800

    add account: go back to all accounts after add

--- app/src/main/java/com/odde/bbuddy/AddAccountActivity.java
+++ app/src/main/java/com/odde/bbuddy/AddAccountActivity.java
@@ -1,7 +1,12 @@
 package com.odde.bbuddy;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+import static android.view.View.*;
 
 public class AddAccountActivity extends AppCompatActivity {
 
@@ -9,5 +14,15 @@ public class AddAccountActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_account);
+
+        Button confirmButton = (Button) findViewById(R.id.confirm);
+        confirmButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), DashboardActivity.class);
+                intent.putExtra("tabPosition", 1);
+                startActivity(intent);
+            }
+        });
     }
 }

21512e182c4e1c384ea78161d912fce73510d8d1
Joseph Yao <joseph.yao.ruozhou@gmail.com>
2017-03-16 17:55:57 +0800

    add budget: implemented with fake ui

---
+++ app/src/main/java/com/odde/bbuddy/budget/AddBudgetActivity.java
@@ -0,0 +1,27 @@
+package com.odde.bbuddy.budget;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+import com.odde.bbuddy.R;
+
+public class AddBudgetActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_add_budget);
+
+        Button saveButton = (Button) findViewById(R.id.confirm);
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/odde/bbuddy/budget/ShowAllBudgetsActivity.java
@@ -0,0 +1,19 @@
+package com.odde.bbuddy.budget;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.odde.bbuddy.R;
+
+public class ShowAllBudgetsActivity extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.activity_budgets, container, false);
+    }
+
+}

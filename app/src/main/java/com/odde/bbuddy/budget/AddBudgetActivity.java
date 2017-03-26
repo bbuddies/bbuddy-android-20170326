2c07c295c91a22e33dea7a62e296d59c94567e90
Joseph Yao <joseph.yao.ruozhou@gmail.com>
2017-03-26 15:00:35 +0800

    add budget: implement list by fake ui

--- app/src/main/java/com/odde/bbuddy/budget/AddBudgetActivity.java
+++ app/src/main/java/com/odde/bbuddy/budget/AddBudgetActivity.java
@@ -3,6 +3,8 @@ package com.odde.bbuddy.budget;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 import com.odde.bbuddy.R;
 
@@ -12,5 +14,13 @@ public class AddBudgetActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_add_budget);
+
+        Button saveButton = (Button) findViewById(R.id.confirm);
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 }
--- app/src/main/java/com/odde/bbuddy/budget/BudgetsActivity.java
+++ app/src/main/java/com/odde/bbuddy/budget/BudgetsActivity.java
@@ -1,6 +1,19 @@
 package com.odde.bbuddy.budget;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.odde.bbuddy.R;
 
 public class BudgetsActivity extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.activity_budgets, null);
+    }
 }

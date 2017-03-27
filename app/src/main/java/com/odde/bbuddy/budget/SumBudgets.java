c4557cbc8305d17a0f30f800c5fe76b730dc18c2
lizhenzhaofirst <707619335@qq.com>
2017-03-28 01:52:46 +0800

    update sumbudget result on ui

---
+++ app/src/main/java/com/odde/bbuddy/budget/SumBudgets.java
@@ -0,0 +1,8 @@
+package com.odde.bbuddy.budget;
+
+/**
+ * Created by lizz on 2017/3/27.
+ */
+
+public class SumBudgets {
+}
---
+++ app/src/main/java/com/odde/bbuddy/common/Singleton.java
@@ -0,0 +1,8 @@
+package com.odde.bbuddy.common;
+
+/**
+ * Created by lizz on 2017/3/28.
+ */
+
+public class Singleton {
+}

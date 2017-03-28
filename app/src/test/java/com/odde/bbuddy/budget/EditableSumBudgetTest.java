e457fc674911043dfcc6e6fcf44dbe4bf04b3b90
lizhenzhaofirst <707619335@qq.com>
2017-03-28 10:59:24 +0800

    commit sum budgets test

--- app/src/test/java/com/odde/bbuddy/budget/EditableSumBudgetTest.java
+++ app/src/test/java/com/odde/bbuddy/budget/EditableSumBudgetTest.java
@@ -6,31 +6,62 @@ import org.junit.Assert;
 import org.junit.Test;
 
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
-
-import static org.mockito.Mockito.mock;
 
 /**
  * Created by lizz on 2017/3/28.
  */
 
 public class EditableSumBudgetTest {
-    BudgetsActivityNavigation mockBudgetsActivityNavigation = mock(BudgetsActivityNavigation.class);
-    BudgetsApi mockBudgetsApi = mock(BudgetsApi.class);
-    EditableSumBudget editableBudget = new EditableSumBudget(mockBudgetsActivityNavigation, mockBudgetsApi);
+    private static final double DELTA = Math.pow(10, -2);
 
     @Test
-    public void test_getMonthPersent() {
+    public void test_getSumBudgets() {
+        List<Budget> budgets = new ArrayList<>();
+        budgets.add(createBudget(1, "2017-02", 1000));
+        budgets.add(createBudget(2, "2017-03", 1000));
+        budgets.add(createBudget(3, "2017-04", 1000));
+
+        float actResult = BudgetSumUtil.getBudgetsSum("2017-02-15", "2017-04-16", budgets);
+        Assert.assertEquals(2033.33, actResult, DELTA);
+    }
+
+    @Test
+    public void test_getSumBudgets_ListEmpty() {
         List<Budget> budgets = new ArrayList<>();
 
-        budgets.add(createBudget(1, "2", 1000));
-        budgets.add(createBudget(2, "3", 1000));
-        budgets.add(createBudget(3, "4", 1000));
+        float actResult = BudgetSumUtil.getBudgetsSum("2017-02-15", "2017-04-16", budgets);
+        Assert.assertEquals(0, actResult, DELTA);
+    }
 
-        float actResult = BudgetSumUtil.getBudgetsSum("2017-2-15", "2017-4-16", budgets);
+    @Test
+    public void test_getSumBudgets_SameMonth() {
+        List<Budget> budgets = new ArrayList<>();
+        budgets.add(createBudget(1, "2017-02", 1000));
 
-        Assert.assertEquals(1000, actResult);
+        float actResult = BudgetSumUtil.getBudgetsSum("2017-02-15", "2017-02-28", budgets);
+        Assert.assertEquals(14 / 28.0f * 1000, actResult, DELTA);
+    }
+
+    @Test
+    public void test_getSumBudgets_OverYear() {
+        List<Budget> budgets = new ArrayList<>();
+        budgets.add(createBudget(1, "2016-12", 1000));
+        budgets.add(createBudget(2, "2017-01", 1000));
+        budgets.add(createBudget(3, "2017-02", 1000));
+
+        float actResult = BudgetSumUtil.getBudgetsSum("2016-12-15", "2017-02-15", budgets);
+        Assert.assertEquals((17 / 31.0f + 15 / 28.0f)* 1000 + 1000, actResult, DELTA);
+    }
+
+    @Test
+    public void test_getSumBudgets_TwoMonths() {
+        List<Budget> budgets = new ArrayList<>();
+        budgets.add(createBudget(1, "2016-12", 1000));
+        budgets.add(createBudget(2, "2017-01", 1000));
+
+        float actResult = BudgetSumUtil.getBudgetsSum("2016-12-15", "2017-01-11", budgets);
+        Assert.assertEquals((17 / 31.0f + 11 / 31.0f)* 1000, actResult, DELTA);
     }
 
     @NonNull
